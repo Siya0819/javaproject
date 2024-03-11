0ddf08fc3bae32d4b16ea9b3177aac6ede305c00
Siya0819 <162999190+Siya0819@users.noreply.github.com>
2024-03-11 19:56:27 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Bharti Rathod! How are you?");  
+     System.out.println("Hello Bharti Rathod! How are you??");  
     }  
 }
